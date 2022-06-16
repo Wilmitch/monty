b62ed4fc62de2a682c524b163b4ae30141c24737
wilmitch <mwangimwilfred@gmail.com>
2022-06-16 04:15:58 +0000

    push pall codes

--- 0-push_opcode.c
+++ 0-push_opcode.c
@@ -9,10 +9,22 @@ void push(stack_t **stack, unsigned int line_number)
 {
 	core_stack_queue(stack, line_number, 0);
 }
+
+/**
+ * stack - Function that will push nodes to the stack
+ * @stack: the structure of the stack
+ * @line_number: Number of instructions
+ */
 void stack(stack_t **stack, unsigned int line_number)
 {
 	core_stack_queue(stack, line_number, 1);
 }
+
+/**
+ * queue - Function that will push nodes to the queue
+ * @stack: structure of the stack
+ * @line_number: number of instructions
+ */
 void queue(stack_t **stack, unsigned int line_number)
 {
 	core_stack_queue(stack, line_number, 2);
